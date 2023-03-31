d18bead16064ea38de60d863be955b0ed0be7db3
Kislitsyna Vasilisa <kislitsyna.vv@phystech.edu>
2023-03-31 10:18:41 +0300

    fixed d and e tasks of hw1

--- 1/e.m
+++ 1/e.m
@@ -1,7 +1,11 @@
-x = linspace(-pi/2, pi/2, 1000);
-y = ( (sin(0.1 .* x) ./ x) .* (sin(20 .* x) ./ sin(x)) ) .^ 2;
+[x, y] = meshgrid(-5:0.1:5,-5:0.1:5);
 
-figure;
-hold on;
-grid on;
-plot(x, y, '.-');
+r = sqrt(x.^2+y.^2);
+z = (besselj(1,0.5*r).^2)./(r.^2);
+
+surf(x, y, z);
+title('Поверхностный график для функции I(x,y)', 'FontSize', 14)
+xlabel('x', 'FontSize', 12)
+ylabel('y', 'FontSize', 12)
+zlabel('I(x,y)', 'FontSize', 12)
+colorbar;
